f71c2e804e49999600b4f1c33ba51fce5e4f40bc
Bilol <madiyarovbilol78@gmail.com>
2025-05-15 16:20:50 +0500

    bug fixed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
 import { join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
